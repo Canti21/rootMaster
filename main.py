e52d193c6360c4faf61b263255d472f3a0cb5062
canti <marsky2431@gmail.com>
2023-09-24 01:46:32 -0600

    Added 404 page and app handler

--- main.py
+++ main.py
@@ -98,6 +98,9 @@ def muller_page():
 
     return render_template('muller.html', resultado=resultado, iterations=iterations, alert=alert)
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
